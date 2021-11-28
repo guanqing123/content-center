cdea42fc10d00f93e96b0f5eb7dda1d17c1c5f52
guanqing <guanqinghz@163.com>
2021-11-29 00:29:00 +0800

    集成nacos,发现内容中心

--- src/main/java/com/itmuch/contentcenter/modular/dev/controller/FansController.java
+++ src/main/java/com/itmuch/contentcenter/modular/dev/controller/FansController.java
@@ -4,6 +4,8 @@ import com.itmuch.contentcenter.modular.dev.model.HyFans;
 import com.itmuch.contentcenter.modular.dev.service.HyFansService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -24,6 +26,9 @@ public class FansController {
 
     private final RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/getFans")
     public List<HyFans> getFans(){
         List<HyFans> hyFans = hyFansService.getFans();
@@ -39,5 +44,16 @@ public class FansController {
                 hyFans.getSex());
         hyFans.setNickName(userStr);
         return hyFans;
+    }
+
+    /**
+     * 测试：服务发现,证明内容中心总能找到用户中心 返回用户中心所有实例的地址信息
+     * @Author guanqing
+     * @Date 2021/11/29 0:20
+     **/
+    @GetMapping("/getInstances")
+    public List<ServiceInstance> getInstances(){
+        // 查询指定服务的所有实例的信息
+        return this.discoveryClient.getInstances("user-center");
     }
 }
