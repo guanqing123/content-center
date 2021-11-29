c9134f166daee8965f4a9fa6b3c35e58f54eb561
guanqing <guanqinghz@163.com>
2021-11-29 23:12:26 +0800

    Ribbon负载均衡策略细粒度配置

---
+++ src/main/java/com/itmuch/contentcenter/config/UserCenterRibbonConfig.java
@@ -0,0 +1,15 @@
+package com.itmuch.contentcenter.config;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Configuration;
+import ribbonConfig.RibbonConfig;
+
+/**
+ * @description: TODO 类描述
+ * @Author guanqing
+ * @Date 2021/11/29 22:54
+ **/
+@Configuration
+@RibbonClient(name = "user-center", configuration = RibbonConfig.class)
+public class UserCenterRibbonConfig {
+}
---
+++ src/main/java/ribbonConfig/RibbonConfig.java
@@ -0,0 +1,19 @@
+package ribbonConfig;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description: TODO 类描述
+ * @Author guanqing
+ * @Date 2021/11/29 23:01
+ **/
+@Configuration
+public class RibbonConfig {
+    @Bean
+    public IRule ribbonRule() {
+        return new RandomRule();
+    }
+}
