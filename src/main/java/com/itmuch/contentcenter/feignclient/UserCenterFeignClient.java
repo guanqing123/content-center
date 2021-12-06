6d8d5168db1abf463629c3ec49bf0bb66fbc25e9
guanqing <guanqinghz@163.com>
2021-12-06 23:46:33 +0800

    feign整合Sentinel & fallback

--- src/main/java/com/itmuch/contentcenter/feignclient/UserCenterFeignClient.java
+++ src/main/java/com/itmuch/contentcenter/feignclient/UserCenterFeignClient.java
@@ -1,6 +1,7 @@
 package com.itmuch.contentcenter.feignclient;
 
 import com.itmuch.contentcenter.config.UserContentFeignConfiguration;
+import com.itmuch.contentcenter.feignclient.fallback.UserCenterFeignClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
  * @Author guanqing
  * @Date 2021/12/1 22:13
  **/
-@FeignClient(name = "user-center")
+@FeignClient(name = "user-center", fallback = UserCenterFeignClientFallback.class)
 public interface UserCenterFeignClient {
     /** @FeignClient(name = "user-center", configuration = UserContentFeignConfiguration.class) */
 
---
+++ src/main/java/com/itmuch/contentcenter/feignclient/fallback/UserCenterFeignClientFallback.java
@@ -0,0 +1,17 @@
+package com.itmuch.contentcenter.feignclient.fallback;
+
+import com.itmuch.contentcenter.feignclient.UserCenterFeignClient;
+import org.springframework.stereotype.Component;
+
+/**
+ * @description: TODO 类描述
+ * @Author guanqing
+ * @Date 2021/12/6 23:39
+ **/
+@Component
+public class UserCenterFeignClientFallback implements UserCenterFeignClient {
+    @Override
+    public String findUserById(String id) {
+        return "一个默认用户";
+    }
+}
