f358a88e5b48accc541fca5b41233f04118b2688
guanqing <guanqinghz@163.com>
2021-12-07 15:40:36 +0800

    sentinel 实现区分来源 RequestOriginParser

---
+++ src/main/java/com/itmuch/contentcenter/sentinel/MyRequestOriginParser.java
@@ -0,0 +1,27 @@
+package com.itmuch.contentcenter.sentinel;
+
+import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.RequestOriginParser;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @description: TODO 类描述
+ * @Author guanqing
+ * @Date 2021/12/7 15:04
+ **/
+@Component
+public class MyRequestOriginParser implements RequestOriginParser {
+    @Override
+    public String parseOrigin(HttpServletRequest request) {
+        /** 从请求参数中获取名为 origin 的参数并返回
+         *  如何获取不到origin参数,那么就抛异常
+         */
+        String origin = request.getParameter("origin");
+        if (StringUtils.isBlank(origin)) {
+            throw new IllegalArgumentException("origin must be specified");
+        }
+        return origin;
+    }
+}
