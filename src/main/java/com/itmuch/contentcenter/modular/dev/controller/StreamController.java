4030ce79fa42e5df2a69a1070f9a5cbbd059c450
guanqing <guanqinghz@163.com>
2021-12-08 11:22:58 +0800

    stream 自定义接口 发送消息

---
+++ src/main/java/com/itmuch/contentcenter/modular/dev/controller/StreamController.java
@@ -0,0 +1,45 @@
+package com.itmuch.contentcenter.modular.dev.controller;
+
+import com.itmuch.contentcenter.rocketmq.MySource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description: TODO stream
+ * @Author guanqing
+ * @Date 2021/12/8 11:20
+ **/
+@RestController
+public class StreamController {
+
+    @Autowired
+    private Source source;
+
+    @GetMapping("/test-stream")
+    public String testStream(){
+        this.source.output()
+                .send(
+                        MessageBuilder
+                                .withPayload("消息体")
+                                .build()
+                );
+        return "success";
+    }
+
+    @Autowired
+    private MySource mySource;
+
+    @GetMapping("/test-my-stream")
+    public String testMyStream(){
+        this.mySource.output()
+                .send(
+                        MessageBuilder
+                                .withPayload("消息体")
+                                .build()
+                );
+        return "success";
+    }
+}
--- src/main/java/com/itmuch/contentcenter/modular/dev/controller/UserController.java
+++ src/main/java/com/itmuch/contentcenter/modular/dev/controller/UserController.java
@@ -3,12 +3,12 @@ package com.itmuch.contentcenter.modular.dev.controller;
 import com.itmuch.contentcenter.modular.dev.model.HyUser;
 import com.itmuch.contentcenter.modular.dev.model.dto.UserDTO;
 import com.itmuch.contentcenter.modular.dev.service.HyUserService;
-import com.itmuch.contentcenter.rocketmq.MySource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @description: TODO 类描述
@@ -31,33 +31,5 @@ public class UserController {
     public HyUser modifyTran(@PathVariable Integer id,@RequestBody UserDTO userDTO){
         // TODO: 认证、授权
         return this.hyUserService.modifyTran(id, userDTO);
-    }
-
-    @Autowired
-    private Source source;
-
-    @GetMapping("/test-stream")
-    public String testStream(){
-        this.source.output()
-                .send(
-                    MessageBuilder
-                        .withPayload("消息体")
-                        .build()
-                );
-        return "success";
-    }
-
-    @Autowired
-    private MySource mySource;
-
-    @GetMapping("/test-my-stream")
-    public String testMyStream(){
-        this.mySource.output()
-                .send(
-                    MessageBuilder
-                        .withPayload("消息体")
-                        .build()
-                );
-        return "success";
     }
 }
